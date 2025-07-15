27d2304056d0c271aa707b7e6adad09e284feba8
Ruckas08 <122235008+Ruckas08@users.noreply.github.com>
2025-07-15 13:01:23 -0400

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,9 @@
+//show/hide MinNav bar from Hamburger menu
+function ShowMinNav(){
+	const MinNav=document.querySelector('.MinNav ul')
+	MinNav.style.display='block'
+}
+function HideMinNav(){
+	const MinNav=document.querySelector('.MinNav ul')
+	MinNav.style.display='none'
+}
